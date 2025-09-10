71889933d070492aecc95ac784fd8a62275eab57
aron-8 <72813559+aron-8@users.noreply.github.com>
2025-09-10 16:05:41 +0000

    from lab

--- main.c
+++ main.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
 
-int main(){
-printf("HELLO!");
-return 0;
+int main()
+{ /*
+//6. Painting
+  float Height;
+  float Diameter;
 
+  printf("Paint the container\nHeight? ");
+  scanf("%f", &Height);
+  printf("Diameter? ");
+  scanf("%f", &Diameter);
+  float area = Height*Diameter*Diameter*3.14159265/4+Height*3.14159265*Diameter;
+  printf("%0.1f can(s) of paint needed.\n", area/2);
+
+//7. \n and %d
+    printf("  /\n /\n\/\n\n");
+    printf("\\\n \\\n  \\\n");
+    char k = '"';
+    char f[3] = {'%', 'd','\0'};
+    printf("printf(%chello\\n%c) will print %chello%c\nand starts a new line\n", k, k, k, k);
+    printf("Using %c%s%c we can print the value of an integer variable.\n", k, f, k);
+
+
+//8. Further problems for practicing
+ int a;
+ int b;
+ printf("Az első?");
+ scanf("%d", &a);
+ printf("Az maásodik?");
+ scanf("%d", &b);
+ int o = a+b;
+ int s = a*b;
+ printf("sum:%d and product:%d", o, s);
+    */
+
+    return 0;
 }
